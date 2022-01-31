8e4249076e46efdb917d34bd3295e8d1b341d4f7
Sárközy Ádám <sarkozy.adam@magnetbank.hu>
2022-01-31 11:55:18 +0100

    Init

--- src/main/java/hu/progmatic/kozos/kassza/BelepesController.java
+++ src/main/java/hu/progmatic/kozos/kassza/BelepesController.java
@@ -2,11 +2,12 @@ package hu.progmatic.kozos.kassza;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BelepesController {
 
-    @GetMapping("/kassza")
+    @RequestMapping("/")
     public String kassza() {
         return "kassza/kezdes";
     }
