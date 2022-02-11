a452467a359375fef15cc78d4a1bce0e15a8b555
krzol <kruppa.zoltan@outlook.hu>
2022-02-11 12:02:35 +0100

    keszpenzdto

--- src/main/java/hu/progmatic/kozos/kassza/KeszpenzDto.java
+++ src/main/java/hu/progmatic/kozos/kassza/KeszpenzDto.java
@@ -1,12 +1,14 @@
 package hu.progmatic.kozos.kassza;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class KeszpenzDto {
 
     private Integer maradek;
