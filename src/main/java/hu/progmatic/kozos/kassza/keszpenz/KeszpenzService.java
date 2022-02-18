c279cf5f7fb3278e8dc72901d72779357231dabf
krzol <kruppa.zoltan@outlook.hu>
2022-02-18 09:34:35 +0100

    keszpenz probalkozas 3.0

--- src/main/java/hu/progmatic/kozos/kassza/keszpenz/KeszpenzService.java
+++ src/main/java/hu/progmatic/kozos/kassza/keszpenz/KeszpenzService.java
@@ -34,6 +34,7 @@ public class KeszpenzService {
         return keszpenzDto;
     }
 
+
     private String visszajaroListToStr(List<Bankjegy> visszajaroList) {
         if (visszajaroList == null) {
             return null;
