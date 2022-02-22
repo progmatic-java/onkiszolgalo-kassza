74984ebf93e6577444134e2baa2aea35a4189220
ptrmagyar <ptrmagyar@gmail.com>
2022-02-22 10:38:57 +0100

    frontend

--- src/main/java/hu/progmatic/kozos/kassza/KosarListaController.java
+++ src/main/java/hu/progmatic/kozos/kassza/KosarListaController.java
@@ -7,24 +7,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.List;
-
 @Controller
-public class AdminControllerKosar {
+public class KosarListaController {
 
     @Autowired
     KosarService kosarService;
 
     @GetMapping("/kassza/admin")
     public String adminFelulet(Model model){
-        model.addAttribute("allKosar", kosarService.findAllKosarViewDto());
-        return "kassza/admin";
+        model.addAttribute("kosar", kosarService.findAllKosarViewDto().stream().findFirst().orElse(KosarViewDTO.builder().build()));
+        return "kassza/kosarlista";
     }
 
-    @PostMapping("/kassza/admin/delete/{kosarId}")
-    public String deleteKosarById(Model model, @PathVariable("kosarId") Integer kosarId){
-        kosarService.deleteKosarById(kosarId);
-        model.addAttribute("allKosar", kosarService.findAllKosarViewDto());
-        return "kassza/admin";
-    }
 }
